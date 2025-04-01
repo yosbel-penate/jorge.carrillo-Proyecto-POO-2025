c2616cd03930fbc7eef2654e536902f92d495a26
Yailen Garcia Panizo <yailengarciap@MacBook-Pro-de-Yailen.local>
2025-04-01 19:53:46 -0400

    Agregado el codigo inicial de TurnController.java

---
+++ TurnController.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+package src.main.java.Controller;
+
+public class TurnController {
+    private int countStepPlayers;
+    private int countStepEnemies;
+
+    public void nextTurn() {}
+}
+
