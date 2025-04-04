14fbe5ffd36a6bad2fc2e865ee78252ff6d41c07
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:22:01 -0400

    Update Adrenaline.java

--- src/main/java/Elements/Items/Adrenaline.java
+++ src/main/java/Elements/Items/Adrenaline.java
@@ -1,3 +1,17 @@
 package src.main.java.Elements.Items
 
-public Adrenaline{
+
+// Clase Adrenalin que hereda de Items 
+class Adrenalin extends Items { 
+  private int increaseInSpeed;
+  
+public Adrenalin(int statsElement, int increaseInSpeed) {
+    super(statsElement);
+    this.increaseInSpeed = increaseInSpeed;
+}
+
+public void enhancementEffect() {
+    System.out.println("La adrenalina aumenta la velocidad en " + increaseInSpeed + " puntos.");
+}
+
+}
