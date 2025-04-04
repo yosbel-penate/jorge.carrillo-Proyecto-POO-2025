595ba360dac6beb9278efcca57fb422277529b66
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:47:05 -0400

    Update Weanpons.java

--- src/main/java/Elements/Weapons/Weanpons.java
+++ src/main/java/Elements/Weapons/Weanpons.java
@@ -10,26 +10,3 @@ public Weapons(int range) {
 
 }
 
-// Clase Flamethrower 
-class Flamethrower extends Weapons { 
-  public Flamethrower(int range) { 
-    super(range); 
-  }
-
-public void fireTorrent() {
-    System.out.println("Lanzallamas emitiendo torrente de fuego.");
-}
-
-}
-
-// Clase PlasmaRifle 
-class PlasmaRifle extends Weapons { 
-  public PlasmaRifle(int range) { 
-    super(range); 
-  }
-
-public void plasmaTorrent() {
-    System.out.println("Rifle de plasma disparando torrente de plasma.");
-}
-
-}
