943736675b03cb4678483680fe339ad4ed92b40d
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:23:16 -0400

    Update Items.java

--- src/main/java/Elements/Items/Items.java
+++ src/main/java/Elements/Items/Items.java
@@ -12,36 +12,6 @@ public abstract void enhancementEffect();
 
 }
 
-// Clase MedicalKit que hereda de Items 
-class MedicalKit extends Items { 
-  private int increaseInLife;
-
-public MedicalKit(int statsElement, int increaseInLife) {
-    super(statsElement);
-    this.increaseInLife = increaseInLife;
-}
-
-public void enhancementEffect() {
-    System.out.println("El botiquín aumenta la vida en " + increaseInLife + " puntos.");
-}
-
-}
-
-
-// Clase Adrenalin que hereda de Items 
-class Adrenalin extends Items { 
-  private int increaseInSpeed;
-  
-public Adrenalin(int statsElement, int increaseInSpeed) {
-    super(statsElement);
-    this.increaseInSpeed = increaseInSpeed;
-}
-
-public void enhancementEffect() {
-    System.out.println("La adrenalina aumenta la velocidad en " + increaseInSpeed + " puntos.");
-}
-
-}
 
 // Clase TurnController 
 class TurnController { 
