d6f8c8a928cbe8292905f528ba7f540e42c74be6
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:37:12 -0400

    Update RemoteHacking.java

--- src/main/java/Elements/Powers/RemoteHacking.java
+++ src/main/java/Elements/Powers/RemoteHacking.java
@@ -1,3 +1,15 @@
 package src.main.java.Elements.Powers
 
-public RemoteHacking{
+// Clase RemoteHacking que hereda de la clase Powers
+class RemoteHacking extends Powers { 
+  
+  public RemoteHacking(int range) { 
+    super(range); 
+  }
+
+public void breakSystemEnemies() {
+    System.out.println("Hackeo remoto desactivando sistemas enemigos.");
+}
+
+}
+
