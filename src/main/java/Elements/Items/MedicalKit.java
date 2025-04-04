c362905e62e30a22e1c6fb423d531e12fe56d776
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:15:53 -0400

    Update MedicalKit.java

--- src/main/java/Elements/Items/MedicalKit.java
+++ src/main/java/Elements/Items/MedicalKit.java
@@ -1,3 +1,17 @@
 package src.main.java.Elements.Items
 
-public MedicalKit{
+// Clase MedicalKit que hereda de Items 
+class MedicalKit extends Items { 
+  private int increaseInLife;
+
+public MedicalKit(int statsElement, int increaseInLife) {
+    super(statsElement);
+    this.increaseInLife = increaseInLife;
+}
+
+public void enhancementEffect() {
+    System.out.println("El botiquín aumenta la vida en " + increaseInLife + " puntos.");
+}
+
+}
+
