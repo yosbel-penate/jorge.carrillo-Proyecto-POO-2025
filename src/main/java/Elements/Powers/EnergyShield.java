7560eefc7670f8c5602ffbfe47cab95f981d2e2b
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 17:36:24 -0400

    Create EnergyShield.java

---
+++ src/main/java/Elements/Powers/EnergyShield.java
@@ -0,0 +1,14 @@
+package src.main.java.Elements.Powers
+
+// Clase EnergyShield que hereda de la clase Powers
+class EnergyShield extends Powers { 
+  
+  public EnergyShield(int range) { 
+    super(range); 
+  }
+
+public void protectPlayers() {
+    System.out.println("Escudo de energía protegiendo jugadores.");
+}
+
+}
