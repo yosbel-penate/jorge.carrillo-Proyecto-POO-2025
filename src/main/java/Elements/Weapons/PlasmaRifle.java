1160c229b885d8757a1297e528bed9c3a871ed0c
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 20:18:10 -0400

    Update PlasmaRifle.java

--- src/main/java/Elements/Weapons/PlasmaRifle.java
+++ src/main/java/Elements/Weapons/PlasmaRifle.java
@@ -1,12 +1,12 @@
 package src.main.java.Elements.Weapons;
 
-// Clase PlasmaRifle 
+// Clase PlasmaRifle
 class PlasmaRifle extends Weapons { 
   public PlasmaRifle(int range) { 
     super(range); 
   }
 
-public void plasmaTorrent() {
+public void PlasmaTorrent() {
     System.out.println("Rifle de plasma disparando torrente de plasma.");
 }
 
