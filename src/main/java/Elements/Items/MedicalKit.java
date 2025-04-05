f706e3c1f8da74bcc2b103c338f4b704bfb8060d
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-04 20:11:02 -0400

    Update MedicalKit.java

--- src/main/java/Elements/Items/MedicalKit.java
+++ src/main/java/Elements/Items/MedicalKit.java
@@ -1,6 +1,6 @@
 package src.main.java.Elements.Items;
 
-// Clase MedicalKit que hereda de Items 
+// Clase MedicalKit que hereda de Items
 class MedicalKit extends Items { 
   private int increaseInLife;
 
