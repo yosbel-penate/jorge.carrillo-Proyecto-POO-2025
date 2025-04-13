ae84b82e7f12af74255d060308942b98322df97e
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-13 08:21:01 -0400

    Update and rename Adrenaline.java to Adrenalin.java

---
+++ src/main/java/Elements/Items/Adrenalin.java
@@ -0,0 +1,37 @@
+package src.main.java.Elements.Items;
+
+// Clase Adrenalin que hereda de Items 
+class Adrenalin extends Items { 
+  private int increaseInSpeed;
+  
+public Adrenalin(int statsElement, int increaseInSpeed) {
+    super(statsElement);
+    this.increaseInSpeed = increaseInSpeed;
+}
+
+public void enhancementEffect(DraLara) {
+    System.out.println("Usando adrenalina...");
+        goal.increaseSpeed(increaseInSpeed);
+   }
+
+public void enhancementEffect(ElenaDrakeCapitan) {
+    System.out.println("Usando adrenalina...");
+        goal.increaseSpeed(increaseInSpeed);
+   }
+
+ public void enhancementEffect(EngineerMarcus) {
+    System.out.println("Usando adrenalina...");
+        goal.increaseSpeed(increaseInSpeed);
+   }
+
+ public void enhancementEffect(JaxKane) {
+    System.out.println("Usando adrenalina...");
+        goal.increaseSpeed(increaseInSpeed);
+   }
+
+ public void enhancementEffect(ZaraQuinn) {
+    System.out.println("Usando adrenalina...");
+        goal.increaseSpeed(increaseInSpeed);
+   }
+  
+}
