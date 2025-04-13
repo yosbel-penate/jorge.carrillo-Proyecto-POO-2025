c6f6ea2bac944dc3ad79236c089515ce07862289
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-04-13 09:01:10 -0400

    Update Weanpons.java

--- src/main/java/Elements/Weapons/Weanpons.java
+++ src/main/java/Elements/Weapons/Weanpons.java
@@ -8,5 +8,17 @@ public Weapons(int range) {
     this.range = range;
 }
 
+public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void showInfo() {
+        System.out.println("Alcance del arma: " + range + " metros.");  
+
 }
 
+}
