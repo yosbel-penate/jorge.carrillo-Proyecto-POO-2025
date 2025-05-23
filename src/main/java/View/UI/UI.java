c63c6e34c839ae959da64f91d3622c1782485414
yailengarc <yailengarciap@gmail.com>
2025-05-23 13:39:41 -0400

    Agregar variable pública cantidadMoneda y método getter en UI.java

--- src/main/java/View/UI/UI.java
+++ src/main/java/View/UI/UI.java
@@ -30,7 +30,7 @@ import static com.almasb.fxgl.dsl.FXGLForKtKt.*;
 
 public class UI {
 
-   static int cantidadMoneda = 0;
+   public static int cantidadMoneda = 0;
     //Images
     Image wikiImage;
     Image buttonWikiImage;
@@ -112,13 +112,14 @@ public class UI {
         iconCoinView.setX(TILE_SIZE  * 27 + 20);
         iconCoinView.setY(TILE_SIZE * 17 + 20);
 
-
         contadorMonedas = new Text();
         contadorMonedas.setText("x " + 0);
         contadorMonedas.setFill(Color.YELLOW);
         contadorMonedas.setFont(Font.font("Negrita",20));
         contadorMonedas.setX(TILE_SIZE * 28);
         contadorMonedas.setY(TILE_SIZE * 18 - 10);
+
+        
 
         latienda = getAssetLoader().loadImage("latienda.png");
         viewLatienda = new ImageView();
@@ -280,6 +281,9 @@ public class UI {
 
     }
 
+        public static int getCantidadMoneda() {
+            return cantidadMoneda;
+}
 
     public void updateCurrentPlayerStats(Entity currentEntity){
         //Caja de informacion
