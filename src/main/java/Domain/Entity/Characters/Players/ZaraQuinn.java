dcd72c289aad53c1cf1d62b101bd28431e0bbcc2
peppapiggy06 <anaesthergonzalez8@gmail.com>
2025-05-24 22:57:01 -0400

    Update ZaraQuinn.java

--- src/main/java/Domain/Entity/Characters/Players/ZaraQuinn.java
+++ src/main/java/Domain/Entity/Characters/Players/ZaraQuinn.java
@@ -1,5 +1,25 @@
 package Domain.Entity.Characters.Players;
 
 public class ZaraQuinn extends Players {
-    public void skillRemoveEnemies() { }
+        public ZaraQuinn() {
+            this.name = "zara";
+            this.atack = 6;
+            this.life = 12;
+            this.cantidadFramesIdle = 5;
+            this.cantidadFramesAtack = 2;
+            this.cantidadFramesCaminando = 8;
+            this.altoFrameAtack = 50;
+            this.anchoFrameAtack = 114;
+            this.altoFramesIdle = 50;
+            this.anchoFramesCaminando = 226;
+            this.anchoFramesIdle = 310;
+            this.altoFramesCaminando = 50;
+            this.cantidaFramesMuerte = 4;
+            this.altoFramesMuerte = 50;
+            this.anchoFramesMuerte = 228;
+            this.cantidadFramesAtackSpecial = 4;
+            this.altoFramesAtackSpecial = 50;
+            this.anchoFramesAtackSpecial = 278;
+            this.hitBox = 11;
+        }
 }
