8ae6b1555f224b60e64f8f9e6eed28a8be33ddba
Vanessa Marli <vanemarli.2703@gmail.com>
2025-06-16 21:38:40 +0000

    new maps view

--- src/main/java/View/Maps/Maps.java
+++ src/main/java/View/Maps/Maps.java
@@ -1,21 +1,20 @@
 package View.Maps;
-import com.almasb.fxgl.entity.level.Level;
 
+import com.almasb.fxgl.entity.level.Level;
 import static com.almasb.fxgl.dsl.FXGL.setLevelFromMap;
 
-//A partir de esta clase se van a crear los mapas de juego
+// Clase para gestionar la carga de mapas del juego
 public class Maps {
-    public static void setLevel1()
-    {
-        Level level = setLevelFromMap("level_01.tmx");
+
+    public static Level setLevel1() {
+        return setLevelFromMap("level_01.tmx");
     }
 
-
-    public static void setLevel2 (){Level level = setLevelFromMap("level_02.tmx");}
-
-    public static void setLevel3() 
-    {
-        Level level = setLevelFromMap("level_03.tmx");
+    public static Level setLevel2() {
+        return setLevelFromMap("level_02.tmx");
     }
 
+    public static Level setLevel3() {
+        return setLevelFromMap("level_03.tmx");
+    }
 }
